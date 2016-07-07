8279481b4f46a698a494db61f50bf183b0ecec78
Viktor Erik Mikael Kronvall <viktor.kronvall@gmail.com>
2016-07-07 21:16:16 +0200

    Implement router using a Stack and an unordered Map.

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -36,24 +36,25 @@ const getComponent = (page : page) : Component<any, any, any> => {
 const delegateTo = (page : Page.page) => (action : Action<any, any>) =>
     Action(Actions.Delegate, Action(page, action));
 
-
-interface StateAttrs {
-    pageStack? : OrderedMap<Page.page, StackRecord>;
-}
-type StackRecord = Record.IRecord<IStackElem>;
-const State = Record<StateAttrs>({
-    pageStack: OrderedMap<Page.page, StackRecord>(),
-});
-
-interface IStackElem {
+interface IPageState {
     state? : any;
 }
 
-const StackElem = (state : any) : Record.Factory<IStackElem> => {
-    return Record<IStackElem>({
+type PageState = Record.IRecord<IPageState>;
+const PageState = (state : any) : Record.Factory<IPageState> => {
+    return Record<IPageState>({
         state,
     });
 }
+interface StateAttrs {
+    states? : OrderedMap<Page.page, PageState>;
+    pageStack? : Stack<Page.page>
+}
+const State = Record<StateAttrs>({
+    pageStack: Stack<Page.page>(),
+    states: Map<Page.page, PageState>(),
+});
+
 
 type state = Record.IRecord<StateAttrs>;
 type navigation = Action<Actions, Page.pushAction>
@@ -65,15 +66,16 @@ type result = Result<state, action>;
 
 export const init = () => {
     const {state: initState, effect: mainEffects} = main.init();
-    const mainPage = StackElem(initState)();
-    let pageStack = OrderedMap<Page.page, StackRecord>([[Page.page.Main, mainPage]]);
-    const state = State({pageStack});
+    const mainPage = PageState(initState)();
+    const pageStack = Stack<Page.page>([Page.page.Main]);
+    const states = Map<Page.page, PageState>([[Page.page.Main, mainPage]]);
+    const state = State({pageStack, states});
     const effect =  mainEffects.map(delegateTo(Page.page.Main));
     return Result(state, effect);
 };
 
 export const update = (state : state, action : action) : result => {
-    const {pageStack} = state;
+    const {pageStack, states} = state;
     const {type} = action;
     if (type === Actions.Navigate) {
         const {data: navAction} = action as navigation;
@@ -82,33 +84,38 @@ export const update = (state : state, action : action) : result => {
             const {type: page, data: pageState} = pageAction;
             const component = getComponent(page);
             const {state: initState, effect} = component.init(pageState);
-            const nextPageState = StackElem(initState)();
-            const newStack = OrderedMap<Page.page, StackRecord>([[page, nextPageState]]).withMutations(map => {
-                pageStack.remove(page).forEach((elem, page) => {
-                    map.set(page, elem);
-                });
+            const nextPageState = PageState(initState)();
+            const newStack = pageStack.push(page);
+            const newStates = states.set(page, nextPageState);
+            const nextState = state.merge({
+                pageStack: newStack,
+                states: newStates,
             });
-            const nextState = state.merge({pageStack: newStack});
-            const t = effect.map(delegateTo(page));
-            return Result(nextState, t);
+            return Result(nextState, effect.map(delegateTo(page)));
         } else if(navAction.type === Page.Actions.PopPage) {
             const {data: pageAction} = navAction as Page.popAction;
-            const page = pageStack.keys().next().value;
-            const newStack = pageStack.delete(page);
-            const nextState = state.merge({pageStack: newStack});
+            const page = pageStack.peek();
+            const newStack = pageStack.pop();
+            const newStates = states.delete(page);
+            const nextState = state.merge({
+                pageStack: newStack,
+                states: newStates,
+            });
             return Result(nextState);
         }
     } else if (type === Actions.Delegate){
         const {data} = action as delegation;
         const {type: page, data: pageAction} = data;
         const component = getComponent(page);
-        const {pageStack} = state;
-        const stackElement = pageStack.get(page);
-        const result = component.update(stackElement.state, pageAction);
+        const {states} = state;
+        const pageState = states.get(page);
+        const result = component.update(pageState.state, pageAction);
         const effect = result.effect.map(delegateTo(page));
-        const newStackElement = StackElem(result.state)();
-        const newStack = pageStack.update(page, (before: any) => newStackElement);
-        const nextState = state.merge({pageStack: newStack});
+        const newPageState = PageState(result.state)();
+        const newStates = states.set(page, newPageState);
+        const nextState = state.merge({
+            states: newStates,
+        });
         return Result(nextState, effect);
     } else {
         throw new Error('Invalid action type in app');
@@ -116,15 +123,16 @@ export const update = (state : state, action : action) : result => {
 };
 
 export const view = (state : state, next : (action : action) => void) => {
-    const {pageStack} = state;
-    const [page, stackElement] = pageStack.entries().next().value;
-    const component : Component<any, any, any> = getComponent(page);
+    const {pageStack, states} = state;
+    const page = pageStack.peek();
+    const pageState = states.get(page);
+    const component = getComponent(page);
     const delegate = (subaction : Action<any, any>) => next(delegateTo(page)(subaction));
     const navigate = (navAction : Page.action) => {
         const navigation = Action(Actions.Navigate, navAction);
         next(navigation as navigation);
     };
-    const content = component.view(stackElement.state, delegate, navigate);
+    const content = component.view(pageState.state, delegate, navigate);
     return (
         <View style={{
             flex:1,
